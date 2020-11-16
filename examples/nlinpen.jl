d65faac6d07743cef5487ba25fe696f7547cace6
MarioB <bencomo.j.mario@gmail.com>
2020-11-16 11:35:50 -0600

    Adding nonlinear pendulum example, with plots.

--- examples/nlinpen.jl
+++ examples/nlinpen.jl
@@ -3,10 +3,13 @@
 
 # Comparing the linearized and adjoint solutions from RRK algs when relaxation
 # parameter γ is viewed as a constant or function of state fields during
-# linearization.
+# linearization. Also, looking at the effects of considering the last time step
+# correction in the linearization.
 
 using AdjRRK
+using LinearAlgebra
 using Plots
+using UnPack
 
 λ=0
 function f(u)
@@ -38,218 +41,347 @@ function Hη(u,δu;adj=false)
     return H*δu
 end
 
-
-rk = rk4
-u0 = [1.5,1]
-
 t0 = 0
 T  = 500
+dt = 0.5
+u0 = [1.5,1]
+
+##RK example
+
+arrks_RK = AdjRRK_struct()
+@pack! arrks_RK = f,df,η,∇η,Hη
+@pack! arrks_RK = u0
+arrks_RK.return_time = true
+arrks_RK.return_Δη = true
+
+ts_RK = Time_struct()
+@pack! ts_RK = t0,T,dt
 
 
-## Computing high res solution with standard RK.
-dt = 0.005
-Time = (t0,T,dt)
+#running RK
+RK_solver!(arrks_RK,ts_RK,rk4)
+u1_RK = arrks_RK.u[1,:]
+u2_RK = arrks_RK.u[2,:]
 
-u,Δη,t = RK_solver(u0,(f,η),Time,rk; return_Δη=true,return_time=true)
-w      = RK_solver((u0,u),(f,df),Time,rk; lin=true)
-z      = RK_solver((u[:,end],u),(f,df),Time,rk; adj=true)
+plot(u1_RK,u2_RK,
+    title="u1 vs u2 (RK)",
+    xlabel="u1",
+    ylabel="u2",
+    legend=false)
+display(plot!())
 
-# display(plot(t,Δη,
-#     title="Change in entropy",
-#     label="RK (dt=$dt)",
-#     xlabel="t",
-#     ylabel="Δη"))
-#
-# display(plot(u[1,:],u[2,:],
-#     title="solution",
-#     label="RK (dt=$dt)",
-#     xlabel="u1",
-#     ylabel="u2"))
-#
-# display(plot(w[1,:],w[2,:],
-#     title="linearized solution",
-#     label="RK (dt=$dt)",
-#     xlabel="w1",
-#     ylabel="w2"))
-#
-# display(plot(z[1,:],z[2,:],
-#     title="adjoint solution",
-#     label="RK (dt=$dt)",
-#     xlabel="z1",
-#     ylabel="z2"))
-
-
-## Computing low res solution with standard RK.
-dt_RK = 0.5
-Time = (t0,T,dt_RK)
-
-u_RK,Δη_RK,t_RK = RK_solver(u0,(f,η),Time,rk; return_Δη=true,return_time=true)
-w_RK            = RK_solver((u0,u_RK),(f,df),Time,rk; lin=true)
-z_RK            = RK_solver((u_RK[:,end],u_RK),(f,df),Time,rk; adj=true)
-
-
-## Computing solution using IDT.
-
-u_IDT,γ_IDT,Δη_IDT = IDT_solver(u0,(f,η,∇η),Time,rk; return_Δη=true)
-w_IDTγ0 = IDT_solver((u0,u_IDT,γ_IDT),(f,df),Time,rk; lin=true,γcnst=true)
-w_IDT   = IDT_solver((u0,u_IDT,γ_IDT),(f,df,∇η,Hη),Time,rk; lin=true)
-z_IDTγ0 = IDT_solver((u_IDT[:,end],u_IDT,γ_IDT),(f,df),Time,rk; adj=true,γcnst=true)
-z_IDT   = IDT_solver((u_IDT[:,end],u_IDT,γ_IDT),(f,df,∇η,Hη),Time,rk; adj=true)
-
-## Computing solution using RRK.
-
-u_RRK,γ_RRK,Δη_RRK,t_RRK,dt_corr = RRK_solver(u0,(f,η,∇η),Time,rk; return_Δη=true, return_time=true)
-w_RRKγ0 = RRK_solver((u0,u_RRK,γ_RRK,dt_corr),(f,df),Time,rk; lin=true,γcnst=true)
-w_RRK   = RRK_solver((u0,u_RRK,γ_RRK,dt_corr),(f,df,∇η,Hη),Time,rk; lin=true)
-z_RRKγ0 = RRK_solver((u_RRK[:,end],u_RRK,γ_RRK,dt_corr),(f,df),Time,rk; adj=true,γcnst=true)
-z_RRK   = RRK_solver((u_RRK[:,end],u_RRK,γ_RRK,dt_corr),(f,df,∇η,Hη),Time,rk; adj=true)
-
-
-## PLOTS
-
-#plotting change in entropy
-plot(title="Change in entropy",
+plot(ts_RK.t,arrks_RK.Δη,
+    title="Entropy conservation (RK)",
     xlabel="t",
-    ylabel="Δη")
-
-plot!(t,Δη,label="RK (dt=$dt)")
-# plot!( t_RK,  Δη_RK, label="RK (dt=$dt_RK)")
-plot!( t_RK, Δη_IDT, label="IDT")
-plot!(t_RRK, Δη_RRK, label="RRK")
+    ylabel="Δη",
+    legend=false)
 display(plot!())
-#png("entropy")
 
-#plotting solutions
-plot(title="solution",
+
+#running lin RK
+arrks_RK.u0_lin = u0
+RK_solver!(arrks_RK,ts_RK,rk4;lin=true)
+w1_RK = arrks_RK.u_lin[1,:]
+w2_RK = arrks_RK.u_lin[2,:]
+
+plot(w1_RK,w2_RK,
+    title="w1 vs w2 (lin. RK)",
+    xlabel="w1",
+    ylabel="w2",
+    legend=false)
+display(plot!())
+
+
+#running adj RK
+arrks_RK.uT_adj = arrks_RK.u[:,end]
+RK_solver!(arrks_RK,ts_RK,rk4;adj=true)
+z1_RK = arrks_RK.u_adj[1,:]
+z2_RK = arrks_RK.u_adj[2,:]
+
+plot(z1_RK,z2_RK,
+    title="z1 vs z2 (adj. RK)",
+    xlabel="z1",
+    ylabel="z2",
+    legend=false)
+display(plot!())
+
+##IDT example
+
+arrks_IDT = AdjRRK_struct()
+@pack! arrks_IDT = f,df,η,∇η,Hη
+@pack! arrks_IDT = u0
+arrks_IDT.return_time = true
+arrks_IDT.return_Δη = true
+
+ts_IDT = Time_struct()
+@pack! ts_IDT = t0,T,dt
+
+
+#running IDT
+IDT_solver!(arrks_IDT,ts_IDT,rk4)
+u1_IDT = arrks_IDT.u[1,:]
+u2_IDT = arrks_IDT.u[2,:]
+
+plot(u1_IDT,u2_IDT,
+    title="u1 vs u2 (IDT)",
     xlabel="u1",
-    ylabel="u2")
-plot!(    u[1,:],    u[2,:], label="RK (dt=$dt)")
-plot!( u_RK[1,:], u_RK[2,:], label="RK (dt=$dt_RK)")
+    ylabel="u2",
+    legend=false)
 display(plot!())
-# png("u_RK")
 
-plot(title="solution",
+plot(ts_IDT.t,arrks_IDT.Δη,
+    title="Entropy conservation (IDT)",
+    xlabel="t",
+    ylabel="Δη",
+    legend=false)
+display(plot!())
+
+
+#running lin IDT (γ0)
+arrks_IDT.γ_cnst = true
+arrks_IDT.u0_lin = u0
+IDT_solver!(arrks_IDT,ts_IDT,rk4;lin=true)
+w1_IDTγ0 = arrks_IDT.u_lin[1,:]
+w2_IDTγ0 = arrks_IDT.u_lin[2,:]
+
+plot(w1_IDTγ0,w2_IDTγ0,
+    title="w1 vs w2 (lin. IDT, γ0)",
+    xlabel="w1",
+    ylabel="w2",
+    legend=false)
+display(plot!())
+
+
+#running lin IDT
+arrks_IDT.γ_cnst = false
+arrks_IDT.u0_lin = u0
+IDT_solver!(arrks_IDT,ts_IDT,rk4;lin=true)
+w1_IDT = arrks_IDT.u_lin[1,:]
+w2_IDT = arrks_IDT.u_lin[2,:]
+
+plot(w1_IDT,w2_IDT,
+    title="w1 vs w2 (lin. IDT)",
+    xlabel="w1",
+    ylabel="w2",
+    legend=false)
+display(plot!())
+
+
+#running adj IDT (γ0)
+arrks_IDT.γ_cnst = true
+arrks_IDT.uT_adj = arrks_IDT.u[:,end]
+IDT_solver!(arrks_IDT,ts_IDT,rk4;adj=true)
+z1_IDTγ0 = arrks_IDT.u_adj[1,:]
+z2_IDTγ0 = arrks_IDT.u_adj[2,:]
+
+plot(z1_IDTγ0,z2_IDTγ0,
+    title="z1 vs z2 (adj. IDT, γ0)",
+    xlabel="z1",
+    ylabel="z2",
+    legend=false)
+display(plot!())
+
+
+#running adj IDT
+arrks_IDT.γ_cnst = false
+arrks_IDT.uT_adj = arrks_IDT.u[:,end]
+IDT_solver!(arrks_IDT,ts_IDT,rk4;adj=true)
+z1_IDT = arrks_IDT.u_adj[1,:]
+z2_IDT = arrks_IDT.u_adj[2,:]
+
+plot(z1_IDT,z2_IDT,
+    title="z1 vs z2 (adj. IDT)",
+    xlabel="z1",
+    ylabel="z2",
+    legend=false)
+display(plot!())
+
+
+##RRK example
+
+arrks_RRK = AdjRRK_struct()
+@pack! arrks_RRK = f,df,η,∇η,Hη
+@pack! arrks_RRK = u0
+arrks_RRK.return_time = true
+arrks_RRK.return_Δη = true
+
+ts_RRK = Time_struct()
+@pack! ts_RRK = t0,T,dt
+
+
+#running RRK
+RRK_solver!(arrks_RRK,ts_RRK,rk4)
+u1_RRK = arrks_RRK.u[1,:]
+u2_RRK = arrks_RRK.u[2,:]
+
+plot(u1_RRK,u2_RRK,
+    title="u1 vs u2 (RRK)",
     xlabel="u1",
-    ylabel="u2")
-plot!(    u[1,:],    u[2,:], label="RK (dt=$dt)")
-plot!(u_IDT[1,:],u_IDT[2,:], label="IDT")
+    ylabel="u2",
+    legend=false)
 display(plot!())
-# png("u_IDT")
 
-plot(title="solution",
-    xlabel="u1",
-    ylabel="u2")
-plot!(    u[1,:],    u[2,:], label="RK (dt=$dt)")
-plot!(u_RRK[1,:],u_RRK[2,:], label="RRK")
+plot(ts_RRK.t,arrks_RRK.Δη,
+    title="Entropy conservation (RRK)",
+    xlabel="t",
+    ylabel="Δη",
+    legend=false)
 display(plot!())
-# png("u_RRK")
 
 
-#plotting linearized solution
-plot(title="linearized solution",
+#running lin RRK (γ0)
+arrks_RRK.γ_cnst = true
+arrks_RRK.u0_lin = u0
+RRK_solver!(arrks_RRK,ts_RRK,rk4;lin=true)
+w1_RRKγ0 = arrks_RRK.u_lin[1,:]
+w2_RRKγ0 = arrks_RRK.u_lin[2,:]
+
+plot(w1_RRKγ0,w2_RRKγ0,
+    title="w1 vs w2 (lin. RRK, γ0)",
     xlabel="w1",
-    ylabel="w2")
-plot!(      w[1,:],      w[2,:], label="RK (dt=$dt)")
-plot!(   w_RK[1,:],   w_RK[2,:], label="RK (dt=$dt_RK)")
+    ylabel="w2",
+    legend=false)
 display(plot!())
-# png("w_RK")
 
-plot(title="linearized solution",
+
+#running lin RRK
+arrks_RRK.γ_cnst = false
+arrks_RRK.u0_lin = u0
+RRK_solver!(arrks_RRK,ts_RRK,rk4;lin=true)
+w1_RRK = arrks_RRK.u_lin[1,:]
+w2_RRK = arrks_RRK.u_lin[2,:]
+
+plot(w1_RRK,w2_RRK,
+    title="w1 vs w2 (lin. RRK)",
     xlabel="w1",
-    ylabel="w2")
-plot!(      w[1,:],      w[2,:], label="RK (dt=$dt)")
-plot!(w_IDTγ0[1,:],w_IDTγ0[2,:], label="IDTγ0")
+    ylabel="w2",
+    legend=false)
 display(plot!())
-# png("w_IDTcnst")
-
-plot(title="linearized solution",
-    xlabel="w1",
-    ylabel="w2")
-plot!(      w[1,:],      w[2,:], label="RK (dt=$dt)")
-plot!(w_RRKγ0[1,:],w_RRKγ0[2,:], label="RRKγ0")
-display(plot!())
-# png("w_RRKcnst")
-
-plot(title="linearized solution",
-    xlabel="w1",
-    ylabel="w2")
-plot!(      w[1,:],      w[2,:], label="RK (dt=$dt)")
-plot!(  w_IDT[1,:],  w_IDT[2,:], label="IDT")
-display(plot!())
-# png("w_IDT")
-
-plot(title="linearized solution",
-    xlabel="w1",
-    ylabel="w2")
-plot!(      w[1,:],      w[2,:], label="RK (dt=$dt)")
-plot!(  w_RRK[1,:],  w_RRK[2,:], label="RRK")
-display(plot!())
-# png("w_RRK")
 
 
-#plotting adjoint solution
-plot(title="adjoint solution",
+#running adj RRK (γ0)
+arrks_RRK.γ_cnst = true
+arrks_RRK.uT_adj = arrks_RRK.u[:,end]
+RRK_solver!(arrks_RRK,ts_RRK,rk4;adj=true)
+z1_RRKγ0 = arrks_RRK.u_adj[1,:]
+z2_RRKγ0 = arrks_RRK.u_adj[2,:]
+
+plot(z1_RRKγ0,z2_RRKγ0,
+    title="z1 vs z2 (adj. RRK, γ0)",
     xlabel="z1",
-    ylabel="z2")
-plot!(      z[1,:],      z[2,:], label="RK (dt=$dt)")
-plot!(   z_RK[1,:],   z_RK[2,:], label="RK (dt=$dt_RK)")
+    ylabel="z2",
+    legend=false)
 display(plot!())
-# png("z_RK")
 
-plot(title="adjoint solution",
+
+#running adj RRK
+arrks_RRK.γ_cnst = false
+arrks_RRK.uT_adj = arrks_RRK.u[:,end]
+RRK_solver!(arrks_RRK,ts_RRK,rk4;adj=true)
+z1_RRK = arrks_RRK.u_adj[1,:]
+z2_RRK = arrks_RRK.u_adj[2,:]
+
+plot(z1_RRK,z2_RRK,
+    title="z1 vs z2 (adj. RRK)",
     xlabel="z1",
-    ylabel="z2")
-plot!(      z[1,:],      z[2,:], label="RK (dt=$dt)")
-plot!(z_IDTγ0[1,:],z_IDTγ0[2,:], label="IDTγ0")
+    ylabel="z2",
+    legend=false)
 display(plot!())
-# png("z_IDTcnst")
 
-plot(title="adjoint solution",
-    xlabel="z1",
-    ylabel="z2")
-plot!(      z[1,:],      z[2,:], label="RK (dt=$dt)")
-plot!(z_RRKγ0[1,:],z_RRKγ0[2,:], label="RRKγ0")
+
+##derivative tests
+
+Nref = 5
+h0 = 2^(-8)
+
+arrks_h = AdjRRK_struct()
+@pack! arrks_h = f,df,η,∇η,Hη
+
+
+##IDT
+arrks_IDT.u0_lin = randn(2)
+
+# assuming γ is cnst in linearization
+arrks_IDT.γ_cnst = true
+arrks_h.γ_cnst = true
+errs_IDTγ0,rate_IDTγ0,h = AdjRRK.derv_test!(IDT_solver!,arrks_IDT,arrks_h,ts_IDT,rk4,h0,Nref)
+
+# with proper linearization
+arrks_IDT.γ_cnst = false
+arrks_h.γ_cnst = false
+errs_IDT,rate_IDT,h = AdjRRK.derv_test!(IDT_solver!,arrks_IDT,arrks_h,ts_IDT,rk4,h0,Nref)
+
+
+#plots
+labels = ["γ const" "proper lin"]
+markers = [:circle :square]
+
+errors = [errs_IDTγ0,errs_IDT]
+plot(h,errors,
+    title="linearization error (IDT)",
+    xlabel="h",
+    ylabel="error",
+    label=labels,
+    marker=markers,
+    xaxis=:log,
+    yaxis=:log)
 display(plot!())
-# png("z_RRKcnst")
 
-plot(title="adjoint solution",
-    xlabel="z1",
-    ylabel="z2")
-plot!(      z[1,:],      z[2,:], label="RK (dt=$dt)")
-plot!(  z_IDT[1,:],  z_IDT[2,:], label="IDT")
+rates = [rate_IDTγ0,rate_IDT]
+plot(rates,
+    title="linearization conv rate (IDT)",
+    xlabel="refinement index",
+    ylabel="rate",
+    label=labels,
+    marker=markers,
+    xaxis=:flip)
 display(plot!())
-# png("z_IDT")
 
-plot(title="adjoint solution",
-    xlabel="z1",
-    ylabel="z2")
-plot!(      z[1,:],      z[2,:], label="RK (dt=$dt)")
-plot!(  z_RRK[1,:],  z_RRK[2,:], label="RRK")
+
+##RRK
+arrks_RRK.u0_lin = randn(2)
+
+# assuming γ is cnst in linearization
+arrks_RRK.γ_cnst = true
+arrks_h.γ_cnst = true
+errs_RRKγ0,rate_RRKγ0,h = AdjRRK.derv_test!(RRK_solver!,arrks_RRK,arrks_h,ts_RRK,rk4,h0,Nref)
+
+# with proper linearization, but const dt
+arrks_RRK.γ_cnst = false
+arrks_RRK.dt_cnst = true
+arrks_h.γ_cnst = false
+arrks_h.dt_cnst = true
+errs_RRKdt0,rate_RRKdt0,h = AdjRRK.derv_test!(RRK_solver!,arrks_RRK,arrks_h,ts_RRK,rk4,h0,Nref)
+
+# with proper linearization
+arrks_RRK.γ_cnst = false
+arrks_RRK.dt_cnst = false
+arrks_h.γ_cnst = false
+arrks_h.dt_cnst = false
+errs_RRK,rate_RRK,h = AdjRRK.derv_test!(RRK_solver!,arrks_RRK,arrks_h,ts_RRK,rk4,h0,Nref)
+
+
+#plots
+labels = ["γ const" "dt const" "proper lin"]
+markers = [:circle :square :star5]
+
+errors = [errs_RRKγ0,errs_RRKdt0,errs_RRK]
+plot(h,errors,
+    title="linearization error (RRK)",
+    xlabel="h",
+    ylabel="error",
+    label=labels,
+    marker=markers,
+    xaxis=:log,
+    yaxis=:log)
 display(plot!())
-#png("z_RRK")
 
-
-# display(plot(t,γ,title="relaxation parameter",legend=false))
-#
-# plot(u_true[1,:],u_true[2,:],title="nonlinear solution",label="RK (dt=0.05)")
-# plot!(u_RK[1,:],u_RK[2,:],label="RK")
-# display(plot!(u_RRK[1,:],u_RRK[2,:],label="RRK"))
-#
-# plot(w_true[1,:],w_true[2,:],title="linearized solution",label="RK (dt=0.05)")
-# plot!(w_RK[1,:],w_RK[2,:],label="RK")
-# plot!(w_RRKγ0[1,:],w_RRKγ0[2,:],label="RRKγ0")
-# display(plot!(w_RRK[1,:],w_RRK[2,:],label="RRK"))
-#
-# plot(z_true[1,:],z_true[2,:],title="adjoint solution",label="RK (dt=0.05)")
-# plot!(z_RK[1,:],z_RK[2,:],label="RK")
-# plot!(z_RRKγ0[1,:],z_RRKγ0[2,:],label="RRKγ0")
-# display(plot!(z_RRK[1,:],z_RRK[2,:],label="RRK"))
-#
-#
-# plot(w_true[1,:],w_true[2,:],title="comparing linearized solutions",label="RK (dt=0.05)")
-# plot!(w_RK[1,:],w_RK[2,:],label="RK")
-# display(plot!(w_RRK[1,:],w_RRK[2,:],label="RRK"))
-#
-# plot(z_true[1,:],z_true[2,:],title="comparing adjoint solutions",label="RK (dt=0.05)")
-# plot!(z_RK[1,:],z_RK[2,:],label="RK")
-# display(plot!(z_RRK[1,:],z_RRK[2,:],label="RRK"))
+rates = [rate_RRKγ0,rate_RRKdt0,rate_RRK]
+plot(rates,
+    title="linearization conv rate (RRK)",
+    xlabel="refinement index",
+    ylabel="rate",
+    label=labels,
+    marker=markers,
+    xaxis=:flip)
+display(plot!())
